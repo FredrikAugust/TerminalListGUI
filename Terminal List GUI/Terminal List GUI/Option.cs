82719fc49a4d1fc9a4a43bb437eae0578439ff30
fredrikaugust <mail.fredrikaugust@gmail.com>
2015-03-04 21:24:38 +0100

    interface and class for option

--- Terminal List GUI/Terminal List GUI/Option.cs
+++ Terminal List GUI/Terminal List GUI/Option.cs
@@ -1,15 +1,45 @@
 ﻿using System;
-using System.Collections.Generic;
 
 namespace Option
 {
     interface IOption
     {
-        
+        string Name { get; set; }
+        string Desc { get; set; }
     }
     public class Option
     {
-        
+        private string name;
+        private string desc;
+
+        public string Name
+        {
+            get
+            { 
+                return this.name;
+            }
+            set
+            {
+                if (value.Length > 1)
+                {
+                    this.name = value;
+                }
+            }
+        }
+        public string Desc
+        {
+            get
+            {
+                return this.desc;
+            }
+            set
+            {
+                if (value.Length > 1)
+                {
+                    this.desc = value;
+                }
+            }
+        }
     }
 
 }
