d76126e2348831818d111c08b8b2e78400276c65
fredrikaugust <mail.fredrikaugust@gmail.com>
2015-03-04 22:29:29 +0100

    can now create options

--- Terminal List GUI/Terminal List GUI/Program.cs
+++ Terminal List GUI/Terminal List GUI/Program.cs
@@ -4,14 +4,60 @@ using System.Text;
 
 namespace TerminalListGUI
 {
+    interface IOption
+    {
+        string Name { get; set; }
+        string Desc { get; set; }
+    }
+    class Option : IOption
+    {
+        private string name;
+        private string desc;
+
+        public string Name
+        {
+            get
+            {
+                return this.name;
+            }
+            set
+            {
+                this.name = value;
+            }
+        }
+
+        public string Desc
+        {
+            get
+            {
+                return this.desc;
+            }
+            set
+            {
+                this.desc = value;
+            }
+        }
+    }
+    class OptionCreator
+    {
+        public static List<Option> CreateOptions()
+        {
+            List<Option> OptionList = new List<Option>();
+
+            Option HelloWorld = new Option();
+            HelloWorld.Name = "Print \"Hello World\"";
+            HelloWorld.Desc = "Prints \"Hello World\"";
+
+            OptionList.Add(HelloWorld);
+
+            return OptionList;
+        }
+    }
     class Program
     {
         static void Main()
         {
-            Option option1 = new Option("test", "test123");
-
-            Console.WriteLine(option1.name);
-            Console.ReadKey();
+            Console.ReadKey();  // Stops the program from exiting when it reaches the last line
         }
     }
 }
