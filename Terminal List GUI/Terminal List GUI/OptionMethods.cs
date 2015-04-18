19aa2daa1bd87c5705990ac4f02c199e3004d31d
mrmadsenmalmo <mail.fredrikaugust@gmail.com>
2015-04-18 03:09:53 +0200

    commented out the entire program

--- Terminal List GUI/Terminal List GUI/OptionMethods.cs
+++ Terminal List GUI/Terminal List GUI/OptionMethods.cs
@@ -7,27 +7,55 @@ using TerminalListGUI;
 
 namespace OptionFunctionManipulator
 {
-    interface IFunction
+    interface IMethod
     {
+        /// <summary>
+        /// This is the interface for the functions which will be associated with the options.
+        /// </summary>
+
         int FunctionID { get; set; }
         void Function();
     }
-    class CreateOptionFunction
+    static class CreateOptionFunction
     {
-        public static List<IFunction> CreateFunctions()
+        /// <summary>
+        /// Instanciates the objects which contains the functions. I am aware this is a klunky way to solve this, but it's the only way for now.
+        /// </summary>
+        /// <returns>List of all the functions which are associated with the options</returns>
+
+        public static List<IMethod> CreateFunctions()
         {
-            List<IFunction> OptionFunctions = new List<IFunction>();
+            List<IMethod> OptionFunctions = new List<IMethod>();
+
+            // To create a new function:
+            // * Start by adding the class, int and method down below (at the bottom of this file)
+            // * Then copy, paste and edit the following commented fields
 
             Exit Exit = new Exit();
             Exit.FunctionID = 0;
+
+            PrintSomething PrintSomething = new PrintSomething();
+            PrintSomething.FunctionID = 1;
+
+            // Class ClassObject = new Class(),
+            // Class.FunctionID = x; Where x is +1 of the highest number over
+
             OptionFunctions.Add(Exit);
+            OptionFunctions.Add(PrintSomething);
+
+            // OptionFunctions.Add(ClassObject);
 
             return OptionFunctions;
         }
     }
-    class ReturnToMenu
+    static class ReturnToMenu
     {
-        public static void FunctionControls() 
+        /// <summary>
+        /// Simple 'controls' that are used when the user has 'entered' a method.
+        /// The only thing you can really do is go back to the "choose method" menu, or exit the application
+        /// </summary>
+
+        public static void MethodControls() 
         {
             string exitInfo = "Press any key to return to the option menu, or Escape to exit the program";
             Console.WriteLine("\n");
@@ -53,7 +81,9 @@ namespace OptionFunctionManipulator
         }
     }
 
-    class Exit : IFunction
+    // Place all of your custom methods under here
+
+    class Exit : IMethod
     {
         private int functionID;
 
@@ -72,6 +102,28 @@ namespace OptionFunctionManipulator
         public void Function()
         {
             Environment.Exit(0);
+        }
+    }
+
+    class PrintSomething : IMethod
+    {
+        private int functionID;
+
+        public int FunctionID
+        {
+            get
+            {
+                return this.functionID;
+            }
+            set
+            {
+                this.functionID = value;
+            }
+        }
+
+        public void Function()
+        {
+            Console.WriteLine("Welcome to paradise!");
         }
     }
 }
--- Terminal List GUI/Terminal List GUI/Program.cs
+++ Terminal List GUI/Terminal List GUI/Program.cs
@@ -7,6 +7,14 @@ namespace TerminalListGUI
 {
     class Option
     {
+        /// <summary>
+        /// Creating necessary variables for the options to work.
+        /// The option ID will be a unique ID that will be used to 
+        /// determine which of the options to show.
+        /// 
+        /// Instances of this class will be used to represent the options.
+        /// </summary>
+
         private int optionID;
         private string name;
         private string desc;
@@ -47,13 +55,17 @@ namespace TerminalListGUI
             }
         }
     }
-    class OptionCreator
+    static class OptionCreator
     {
-        private static void AddToList(Option _optionToAdd, List<Option> _optionsList)
-        {
-            _optionsList.Add(_optionToAdd);
-        }
-        public static void CreateOption(Option _option, List<Option> _list,  string _name, string _desc, int _id = 1)
+        /// <summary>
+        /// This features all the methods necessary to create an option and it's components.
+        /// </summary>
+        /// <param name="_option">The option which the method should edit/create</param>
+        /// <param name="_list">The list which the option will be added to</param>
+        /// <param name="_name">The name of the option</param>
+        /// <param name="_desc">A short one-liner description of the option and what it will do</param>
+
+        public static void CreateOption(Option _option, List<Option> _list,  string _name, string _desc)
         {
             _option.Name = _name;
             _option.Desc = _desc;
@@ -74,11 +86,20 @@ namespace TerminalListGUI
                 _option.OptionID = 1;
             }
 
-            AddToList(_option, _list);
+            _list.Add(_option);
         }
     }
-    class MenuHandler
+    static class MenuHandler
     {
+        /// <summary>
+        /// This simply handles the navitgation of the options. 
+        /// Regarding of the user input this method will see if you are able to move up/down
+        /// if so; you will move up or down.
+        /// </summary>
+        /// <param name="_position">This is the parameter that determines where in the option "list" you are</param>
+        /// <param name="_direction">Which direction the user is trying to move in</param>
+        /// <param name="_options">This is a list of all the options, I might incorporate this into some other method later</param>
+        
         private static void ChangeOption(int _position, string _direction, List<Option> _options)
         {
             if (_direction == "up" && _position != 0)
@@ -90,26 +111,41 @@ namespace TerminalListGUI
                 MenuUI.CreateMenu(_position + 1);
             }
         }
-        private static void CallFunction(int _pos, List<Option> _options)
-        {
-            List<IFunction> OptionFunctions = CreateOptionFunction.CreateFunctions();
 
-            for (int i = 0; i < OptionFunctions.Count; i++)
+        /// <summary>
+        /// Depending on the cursors position in the option list, it will call the respective function
+        /// </summary>
+        /// <param name="_pos">The position of the cursor. This is retrieved from another method</param>
+        /// <param name="_options">A list of all the OPTIONS, it is very important that this is paralell with the METHOD list</param>
+
+        private static void CallMethod(int _pos, List<Option> _options)
+        {
+            List<IMethod> OptionMethods = CreateOptionFunction.CreateFunctions();  // This is where all the functions belonging to the options are stored for now
+
+            for (int i = 0; i < OptionMethods.Count; i++)
             {
-                if (OptionFunctions[i].FunctionID == _pos)
+                if (OptionMethods[i].FunctionID == _pos)
                 {
-                    OptionFunctions[i].Function();
-                    ReturnToMenu.FunctionControls();
+                    OptionMethods[i].Function();
+                    ReturnToMenu.MethodControls();
                 }
             }
         }
+
+        /// <summary>
+        /// This gets the key the user pressed, and passes that value along to the correct method.
+        /// I have not done any error handling etc. 
+        /// </summary>
+        /// <param name="options">Once again; the list of options</param>
+        /// <param name="position">The position in the list of options</param>
+
         public static void ReadKey(List<Option> options, int position)
         {
             Console.WriteLine("Use the arrow keys to navigate, and enter to select option");
 
             Console.CursorVisible = false;
 
-            while (true)
+            while (true)  // This runs an infinite loop, which means that this could really just be in the main loop of the Program class.
             {
                 ConsoleKeyInfo keyPressed = Console.ReadKey(true);
 
@@ -122,7 +158,7 @@ namespace TerminalListGUI
                         ChangeOption(position, "down", options);
                         continue;
                     case "Enter":
-                        CallFunction(position, options);
+                        CallMethod(position, options);
                         break;
                     default:
                         Console.WriteLine("Invalid input, try again.");
@@ -131,20 +167,40 @@ namespace TerminalListGUI
             }
         }
     }
-    class MenuUI
+    static class MenuUI
     {
-        private static void PrintMenuOptions(Option option)
+        /// <summary>
+        /// This will print out a single option to the screen
+        /// This is put in a separate method simply for syntactic sugar, meaning that it doesn't affect the program either way, 
+        /// but I think this approach looks better.
+        /// </summary>
+        /// <param name="option">A single option that this will print out</param>
+
+        private static void PrintMenuOption(Option option)
         {
             Console.WriteLine("Name: {0}".PadRight(Console.WindowWidth - (1 + option.Name.Length)), option.Name);
             Console.WriteLine("Desc: {0}".PadRight(Console.WindowWidth - (1 + option.Desc.Length)), option.Desc);
             Console.WriteLine();
         }
+
+        /// <summary>
+        /// Creates the options menu, this is done by simply printing the name and the desription followed by an empty line.
+        /// The only logic in this method is the fact that it will print a 'formatted' line when printing the currently selected option.
+        /// This is done using the Console object, I might go over to a more homemade design later using
+        /// Windows Forms as this is a bit more flexible and easier maintainable.
+        /// </summary>
+        /// <param name="selected">The currently selected option, this is used for displaying what option is currently selected</param>
+        
         public static void CreateMenu(int selected = 0)
         {
             List<Option> Options = new List<Option>();
 
             Option Exit = new Option();
             OptionCreator.CreateOption(Exit, Options, "Exit", "Terminates the program");
+
+            Option PrintSomething = new Option();
+            OptionCreator.CreateOption(PrintSomething, Options, "Print Something", "Prints a magnificent piece of art");
+
             Console.Clear();
 
             for (int i = 0; i < Options.Count; i++)
@@ -154,14 +210,14 @@ namespace TerminalListGUI
                     Console.BackgroundColor = ConsoleColor.White;
                     Console.ForegroundColor = ConsoleColor.Black;
 
-                    PrintMenuOptions(Options[i]);
+                    PrintMenuOption(Options[i]);
                     
                     Console.ResetColor();
 
                     continue;
                 }
 
-                PrintMenuOptions(Options[i]);
+                PrintMenuOption(Options[i]);
             }
 
             MenuHandler.ReadKey(Options, selected);
@@ -169,9 +225,15 @@ namespace TerminalListGUI
     }
     class Program
     {
+        /// <summary>
+        /// Because of the pretty horrible structure of methods in this program, this will start the CreateMenu() method, 
+        /// which will eventually result in an infinite loop checking for user input. 
+        /// Once again; this is one of the reasons for why I'm considering changing to Windows Forms
+        /// </summary>
+
         static void Main()
         {
-            MenuUI.CreateMenu();  // MAKE MAGIC HAPPEN
+            MenuUI.CreateMenu();
         }
     }
 }
